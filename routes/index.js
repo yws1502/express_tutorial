2209ac0b14aff8ab42ab5639d284cc60550155fc
Woosang <yws1502@naver.com>
2021-12-27 15:46:00 +0900

    ✨ Create http method and routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let arr = [];
+
+// GET method
+
+router.get('/read', (req, res) => {
+  res.status(200).json({
+    message: "read success",
+  });
+});
+
+// POST method
+
+router.post('/create', (req, res) => {
+  const data = req.body.data;
+  arr.push(data);
+  res.status(200).json({
+    message: "create success",
+    result: arr,
+  });
+});
+
+// PUT method
+
+router.put('/update/:id', (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  arr[id] = data;
+  res.status(200).json({
+    message: "update success",
+    result: arr,
+  });
+});
+
+// DELETE method
+
+router.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
+  arr.splice(id, 1);
+  res.status(200).json({
+    message: "delete success",
+    result: arr,
+  });
 });
 
 module.exports = router;
