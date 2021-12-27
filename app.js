525d177df116c22c88d1de6be3c24d0b78d3a72d
Woosang <yws1502@naver.com>
2021-12-27 16:47:32 +0900

    Setting an express-session in app.js

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const templateRouter = require('./routes/template');
 
+const session = require('express-session');
+
 var app = express();
+
+app.use(
+  session({
+  secret: "first project", // 임의로 작성하면 되지만 되도록이면 숨김 파일 만들어서 import 하는 형식으로 사용하자!
+  resave: false, // session을 변경하지 않아도 저장할지 안할지는 정하는 것 false == session이 변경되지 않을 때는 저장 X
+  saveUninitialized: true, // session을 저장되기 전에 이를 초기화할지 말지를 정하는 것
+  })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
